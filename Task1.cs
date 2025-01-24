32cb1c69c52f277cc9ad316be759620955afed35
Makayla-ALD <ma654968@ucf.edu>
2025-01-23 19:53:38 -0500

    Task 1 assignment

--- Task1.cs
+++ Task1.cs
@@ -5,44 +5,55 @@ using UnityEngine;
 public class Task1 : MonoBehaviour
 {
     //Goal is to have user calculate the difficulty of the course they inputed
-     public string courseName;
+    public string courseName;
     //Input number for factors can be 1 through 10
     [Range(1f, 10f)] public float numberModules;
     [Range(1f, 10f)] public float numberMaterials;
     [Range(1f, 10f)] public float numberQuizzes;
     [Range(1f, 10f)] public float numberAssignments;
-    public bool instructorExperience = true;
+    public bool instructorExperience;
     //Instructor experience will an if statement
-    private float numberResult = 1.0f;
+    private float numberResult;
 
     private float numberModulePr = 0.15f;
     private float numberMaterialsPr = 0.3f;
     private float numberQuizzesPr = 0.15f;
     private float numberAssignmentsPr = 0.30f;
-    private float instructorExperiencePr = 0.1f;
 
     //Inputed number will be multiplied by the factor's decimal version of the precentage
     void Start()
-     {
-        Debug.Log(result());
-        Debug.Log("Course name: " + courseName + " Challenge score: " + numberResult + ".");
-
-    }
-  private float instr(bool weight, float height) 
     {
-        if (weight == true) return height; else {return 0;}
+        Debug.Log("Course name: " + courseName + " Challenge score: " + result() + ".");
+    }
+
+    private float temp(bool weight)
+    {
+        if (weight == true)
+        {
+            return 1.0f;
+        }
+        else
+        {
+            return 0.0f;
+        }
     }
 
     private float ahhhh(float weight, float height)
     {
-        return weight * height; 
+        return weight * height;
     }
- 
-   private float result()
+
+    private float result()
     {
-        int returnVariable;
-        returnVariable = (int)(numberResult += ((ahhhh(numberModules, numberModulePr)) + (ahhhh(numberMaterials, numberMaterialsPr)) + (ahhhh(numberQuizzes, numberQuizzesPr)) + (ahhhh(numberAssignments, numberAssignmentsPr)) + (instr(instructorExperience, instructorExperiencePr))));
-        return returnVariable;
+        float numberResult = 1;
+        numberResult += ahhhh(numberModules, numberModulePr);
+        numberResult += ahhhh(numberMaterials, numberMaterialsPr);
+        numberResult += ahhhh(numberQuizzes, numberQuizzesPr);
+        numberResult += ahhhh(numberAssignments, numberAssignmentsPr);
+        numberResult += temp(instructorExperience);
+
+        return (int)numberResult;
+
     }
-    
-    }
+
+}
